3325828d694e79f421ac2b3ae4077e4dac0edfdb
Christian Zöller <noex_home@yahoo.de>
2017-01-28 18:29:24 +0100

    Removed old tests

--- src/test/java/de/no3x/countercascade/ModCounterTest.java
+++ src/test/java/de/no3x/countercascade/ModCounterTest.java
@@ -2,9 +2,6 @@ package de.no3x.countercascade;
 
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -35,22 +32,5 @@ public class ModCounterTest {
         assertThat( modCounter.getCount(), is(1) );
         modCounter.decrement();
         assertThat( modCounter.getCount(), is(0) );
-    }
-
-    @Test
-    public void discreteTests() throws Exception {
-        Function<Integer, Integer> f = t -> ( (int) Math.ceil( (t+1)/60.)) % 60;
-
-        for (int i = 0; i <= TimeUnit.MINUTES.toSeconds(1)-1; i++) {
-            System.out.println(f.apply(i));
-            assertThat( f.apply(i), is(0));
-        }
-
-        for (int i = 60; i <= TimeUnit.MINUTES.toSeconds(2)-1; i++) {
-            assertThat( f.apply(i), is(1));
-        }
-
-        assertThat( f.apply( (int) TimeUnit.MINUTES.toSeconds(1)), is(1));
-        assertThat( f.apply( (int) TimeUnit.MINUTES.toSeconds(2)), is(2));
     }
 }
